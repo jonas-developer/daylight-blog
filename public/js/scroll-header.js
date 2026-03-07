5943cca2ec3eea4253acd9c9fc3c83073776261b
Osgar Developer <osgar@webinno.io>
2026-03-07 11:58:45 +0100

    Add scroll-triggered 50% transparency to hero image

---
+++ public/js/scroll-header.js
@@ -0,0 +1,11 @@
+// Add scrolled class to header when page is scrolled
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('.site-header');
+  if (header) {
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  }
+});
